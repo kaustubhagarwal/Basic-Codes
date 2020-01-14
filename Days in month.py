525ebeaaefc69851e820a70c72bb93321d561e14
kaustubhagarwal <34659241+kaustubhagarwal@users.noreply.github.com>
2020-01-15 00:23:26 +0530

    Days in month
    Enter month in words and get the output as the number of days in that month

---
+++ Days in month.py
@@ -0,0 +1,7 @@
+l1=["January","February","March","April","May","June","July","August","September","October","November","December"]
+l2=[31,28,31,30,31,30,31,31,30,31,30,31]
+l=input("Enter the month ")
+c=0
+for i in range(12):
+    if(l in l1[i]):
+        print("Number of days in "+str(l1[i])+" is "+str(l2[i]))
